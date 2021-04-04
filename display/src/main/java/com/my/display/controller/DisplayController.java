f8e44cd7aa505a21a1761bba962bf9ff82fabc8e
taekrigi <taekrigi@gmail.com>
2021-04-04 17:42:28 +0900

    added lombok and devtools

--- display/src/main/java/com/my/display/controller/DisplayController.java
+++ display/src/main/java/com/my/display/controller/DisplayController.java
@@ -4,12 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("display")
+@RequiredArgsConstructor
 public class DisplayController {
 
 	@GetMapping
 	public String displayProduct() {
-		return "Hello World";
+		return "Hello World!!";
 	}
 }
--- product/src/main/java/com/my/product/controller/ProductController.java
+++ product/src/main/java/com/my/product/controller/ProductController.java
@@ -1,5 +1,18 @@
 package com.my.product.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("product")
+@RequiredArgsConstructor
 public class ProductController {
 
+	@GetMapping
+	public String getProduct() {
+		return "Hello Product!";
+	}
 }
