8402f9c045a2d57c36ec777523e840029bbaacf3
taekrigi <taekrigi@gmail.com>
2021-04-04 17:23:32 +0900

    init

---
+++ display/src/main/java/com/my/display/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.my.display.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("main")
+public class MainController {
+
+	@GetMapping
+	public String home() {
+		return "Hello World";
+	}
+}
